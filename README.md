e102ca4e44b216559985390367f9f71d67b82f62
Wattox00 <korschzozinho@gmail.com>
2023-10-30 19:38:47 +0100

    Files

---
+++ README.md
@@ -0,0 +1,47 @@
+# IP Subnet Calculator and IP Subnet Calculation Practise
+
+A simple Python program for calculating network details or practising their calculations.
+
+## Prerequisites
+
+Before you begin, make sure you have the following installed on your computer:
+
+- [Visual Studio Code](https://code.visualstudio.com/download)
+- [Python](https://www.python.org)
+- Python extension in Visual Studio Code
+
+## Getting Started
+
+### Installation
+
+1. **Download and Install Visual Studio Code:**
+   - If you haven't already, download and install Visual Studio Code from the official website: [Download VS Code](https://code.visualstudio.com/download).
+
+2. **Download the Program:**
+   - Visit our [GitHub repository](https://github.com/WattoX00/Ip) and click the "Code" button, then select "Download ZIP."
+
+3. **Extract the ZIP File:**
+   - After the download is complete, extract the contents of the ZIP file to a location of your choice on your computer.
+
+### Running the Program
+
+1. **Open Visual Studio Code:**
+   - Launch Visual Studio Code on your computer.
+
+2. **Open the Project:**
+   - Use the "File" > "Open Folder" option in Visual Studio Code and select the folder where you extracted the ZIP file.
+
+3. **Run the Program:**
+   - Open the Python script in Visual Studio Code and run the program following the provided instructions.
+
+## Program Usage
+
+- Enter an IP address, and the program will calculate the network, first and last IP, broadcast, and display the binary representation of the last octets.
+
+## Support and Contact
+
+Feel free to reach out if you have any questions or encounter issues with the code. You can [open an issue](https://github.com/WattoX00/Ip) on our GitHub repository or contact us directly.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE) - see the [LICENSE](LICENSE) file for details.
---
+++ networking/ip_subnet_calculator/ip_subnetmask_calculator.py
@@ -0,0 +1,157 @@
+'''
+    Ip Subnet Calculator (A,B,C)
+    
+    #! FOR VISUAL STUDIO CODE EDITOR
+
+    Author: WattoX00
+    Date:   30/10/2023
+'''
+
+'''
+Ip format:
+x.y.z.w/mask
+e.g. 192.168.100.1/24
+'''
+print("Format: e.g., 192.168.100.1/24")
+def calculator():
+    try:
+        raw_ip = str(input("ip+mask: "))
+        octet_decimal = int()
+        mask = raw_ip.split("/")
+        if len(mask) != 2:
+                raise ValueError("Invalid input format. Please use IP+Mask format.")
+        ip = mask[0].split(".")
+        mask=int(mask[1])
+        
+        x_width = 15
+        x = int()
+        network = int()
+        broadcast = int()
+        first_ip = int()
+        last_ip = int()
+        bit = [128,64,32,16,8,4,2,1]
+        
+        if mask > 0 and mask < 31:
+            if mask >= 24:
+                bit_needed = mask-24
+                for i in range(bit_needed):
+                    octet_decimal+=bit[i]
+                steps = 256-octet_decimal
+                octet = int(ip[-1])
+                count=True
+
+                start_ip = str()
+                for e in range(len(ip)-1):
+                    start_ip += ip[e]+"."
+
+                while count:
+                    if octet >= steps*x and octet <= steps*(x+1):
+                        network = steps*x
+                        network_bin = bin(network)
+                        broadcast = (steps*(x+1))-1
+                        broadcast_bin = bin(broadcast)
+                        first_ip = network+1
+                        first_bin = bin(first_ip)
+                        last_ip = broadcast-1
+                        last_bin = bin(last_ip)
+
+                        network = str(start_ip)+str(network).ljust(x_width)+"\t"
+                        broadcast = str(start_ip)+str(broadcast).ljust(x_width)+"\t"
+                        first_ip = str(start_ip)+str(first_ip).ljust(x_width)+"\t"
+                        last_ip = str(start_ip)+str(last_ip).ljust(x_width)+"\t"
+
+                        print("Network: \033[34m"+network,network_bin+"\n"
+                            "\033[0mBroadcast: \033[34m"+broadcast,broadcast_bin+"\n"
+                            "\033[0mFirst Ip: \033[34m"+first_ip,first_bin+"\n"
+                            "\033[0mLast Ip: \033[34m"+last_ip,last_bin+"\033[0m")
+                        count=False
+                    else:
+                        x+=1
+            elif mask >= 16:
+                bit_needed = mask-16
+                for i in range(bit_needed):
+                    octet_decimal+=bit[i]
+                steps = 256-octet_decimal
+                octet = int(ip[-2])
+                count=True
+
+                start_ip = str()
+                for e in range(len(ip)-2):
+                    start_ip += ip[e]+"."
+
+                while count:
+
+                    if octet >= steps*x and octet <= steps*(x+1):
+                        network = steps*x
+                        broadcast = (steps*(x+1))-1
+                        first_ip = network
+                        last_ip = broadcast
+                        print("Network: \033[34m"+str(start_ip)+str(network)+".0".ljust(x_width)+"\t","00000000"+"\n"
+                            "\033[0mBroadcast: \033[34m"+str(start_ip)+str(broadcast)+".255".ljust(x_width)+"\t","11111111"+"\n"
+                            "\033[0mFirst Ip: \033[34m"+str(start_ip)+str(first_ip)+".1".ljust(x_width)+"\t","00000001"+"\n"
+                            "\033[0mLast Ip: \033[34m"+str(start_ip)+str(last_ip)+".254".ljust(x_width)+"\t","11111110"+"\033[0m")
+                        count=False
+
+                    else:
+                        x+=1
+            elif mask >= 8:
+                bit_needed = mask-8
+                for i in range(bit_needed):
+                    octet_decimal+=bit[i]
+                steps = 256-octet_decimal
+                octet = int(ip[-3])
+                count=True
+
+                start_ip = str()
+                for e in range(len(ip)-3):
+                    start_ip += ip[e]+"."
+
+                while count:
+
+                    if octet >= steps*x and octet <= steps*(x+1):
+                        network = steps*x
+                        broadcast = (steps*(x+1))-1
+                        first_ip = network
+                        last_ip = broadcast
+                        print("Network: \033[34m"+str(start_ip)+str(network)+".0.0".ljust(x_width)+"\t","00000000"+"\n"
+                            "\033[0mBroadcast: \033[34m"+str(start_ip)+str(broadcast)+".255.255".ljust(x_width),"11111111"+"\n"
+                            "\033[0mFirst Ip: \033[34m"+str(start_ip)+str(first_ip)+".0.1".ljust(x_width)+"\t","00000001"+"\n"
+                            "\033[0mLast Ip: \033[34m"+str(start_ip)+str(last_ip)+".255.254".ljust(x_width)+"\t","11111110"+"\033[0m")
+                        count=False
+
+                    else:
+                        x+=1
+            elif mask >= 0:
+                bit_needed = mask-0
+                for i in range(bit_needed):
+                    octet_decimal+=bit[i]
+                steps = 256-octet_decimal
+                octet = int(ip[-4])
+                count=True
+
+                start_ip = str()
+                for e in range(len(ip)-4):
+                    start_ip += ip[e]+"."
+
+                while count:
+
+                    if octet >= steps*x and octet <= steps*(x+1):
+                        network = steps*x
+                        broadcast = (steps*(x+1))-1
+                        first_ip = network
+                        last_ip = broadcast
+                        print("Network: \033[34m"+str(start_ip)+str(network)+".0.0.0".ljust(x_width)+"\t","00000000"+"\n"
+                            "\033[0mBroadcast: \033[34m"+str(start_ip)+str(broadcast)+".255.255.255".ljust(x_width)+"\t","11111111"+"\n"
+                            "\033[0mFirst Ip: \033[34m"+str(start_ip)+str(first_ip)+".0.0.1".ljust(x_width)+"\t","00000001"+"\n"
+                            "\033[0mLast Ip: \033[34m"+str(start_ip)+str(last_ip)+".255.255.254".ljust(x_width)+"\t","11111110"+"\033[0m")
+                        count=False
+
+                    else:
+                        x+=1
+        else:
+            print("Wrong mask!")
+    except KeyboardInterrupt:
+        print("Exiting the program.")
+        quit()
+while True:
+    calculator()
